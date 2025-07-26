7cb04bb22b515ae15c676fcae19714f891690a7a
Limah-T <limahtechnology@yahoo.com>
2025-07-26 05:43:45 +0100

    Write code for the Instructor views and URL routing, and test all http endpoints (get, post, put, patch. delete) via postman.

--- ai_academy/courses/urls.py
+++ ai_academy/courses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TopicModelViewSet
+from .views import TopicModelViewSet, InstructorViewSet
 
 urlpatterns = [
     path('topics/', TopicModelViewSet.as_view({'get': 'list', 'post': 'create'}), name='topic-list'),
     path('topics/<int:pk>/', TopicModelViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='topic-detail'),
+    path('instructors/', InstructorViewSet.as_view({'get': 'list', 'post': 'create'}), name='instructor'),
+    path('instructors/<int:pk>/', InstructorViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'update', 'delete': 'destroy'}), name='instructor')
 ]
--- ai_academy/courses/views.py
+++ ai_academy/courses/views.py
@@ -35,8 +35,6 @@ class TopicModelViewSet(viewsets.ModelViewSet):
     # @permission_required('user.change_topic', raise_exception=True)
     def update(self, request, *args, **kwargs):
         instance = get_object_or_404(self.queryset, pk=kwargs.get('pk'))
-        if not instance:
-            return Response({"details": "Topic not found"}, status=status.HTTP_404_NOT_FOUND)
         serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         topic = serializer.save()
@@ -46,8 +44,49 @@ class TopicModelViewSet(viewsets.ModelViewSet):
     # @permission_required('user.delete_topic', raise_exception=True)
     def destroy(self, request, *args, **kwargs):
         instance = get_object_or_404(self.queryset, pk=kwargs.get('pk'))
-        if not instance:
-            return Response({"detail": "Topic not found"}, status=status.HTTP_404_NOT_FOUND)
         instance.delete()
         return Response({"success": "Successfully deleted a topic."}, status=status.HTTP_204_NO_CONTENT)
 
+class InstructorViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+    permission_classes = []
+    http_method_names = ["get", "post", "put", "patch", "delete"]
+    queryset = Instructor.objects.all()
+    serializer_class = InstructorSerializer
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.queryset, many=True)
+        return Response({"success": "Successfully retrieved all Instructors",
+                        "data": serializer.data}, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, *args, **kwargs):
+        instructor = get_object_or_404(Instructor, id=kwargs.get('pk'))
+        serializer = self.get_serializer(instructor)
+        return Response({"success": "Successfully retrieved an Instructor",
+                         "data": serializer.data}, status=status.HTTP_200_OK)
+    
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"success": "Successfully added new Instructor",
+                         "data": serializer.data}, status=status.HTTP_201_CREATED)
+    
+    # @permission_required()
+    def update(self, request, *args, **kwargs):
+        instructor = get_object_or_404(Instructor, id=kwargs.get('pk'))
+        serializer = self.get_serializer(instance=instructor, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"success": "Successfully updated an Instructor instance",
+                         "data": serializer.data}, status=status.HTTP_202_ACCEPTED)
+    
+    # @permission_required()
+    def destroy(self, request, *args, **kwargs):
+        instructor = get_object_or_404(Instructor, id=kwargs.get('pk'))
+        instructor.delete()
+        return Response({"success": "Successfully deleted an Instructor."}, status=status.HTTP_204_NO_CONTENT)
+    
+
+    
